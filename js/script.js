8bb4fac192986fb04e0d9b8a05191e3821ca15d9
dev-nirob1 <dev.nirob1@gmail.com>
2025-02-28 20:32:57 +0600

    home page slider initialized

--- js/script.js
+++ js/script.js
@@ -11,3 +11,17 @@ var swiper = new Swiper(".mySwiper", {
         prevEl: ".swiper-button-prev",
     }
 });
+var swiper2 = new Swiper(".homeSwiper", {
+    slidesPerView: "auto",
+    loop: true,
+    spaceBetween: 0,
+    effect: "fade",
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    }
+});
